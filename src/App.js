18952656ae0b36fd51fb60eb43fc5ef9298d113b
steven <stevenbaezfernandez@gmail.com>
2022-03-13 19:02:32 -0400

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [ counter, setCounter ] = useState(0);
+  const [ squareVisibility, setSquareVisibility ] = useState(true);
+  const [ inputValue, setInputValue ] = useState('steven');
+
+  let handlerChange = (event)=>{
+    setInputValue(event.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Hello word!</h1>
+        <span className='counter'>{counter}</span>
+        
+        <div className="wraper-btns">
+          <button onClick={() => setCounter(prevState => prevState + 1)}>Increment</button>
+          <button onClick={() => setCounter(prevState => prevState = 0)}>Reset</button>
+          <button onClick={() => setCounter(prevState => prevState - 1)}>Decrement</button>
+        </div>
+        <div className={`${squareVisibility? 'active' : ''} block`}>Block</div>
+        <button className='btn-toggle-squere' onClick={()=> setSquareVisibility(prevState => !prevState )}>Toggle square</button>
+
+        <input 
+        placeholder='enter something...' 
+        id='something' 
+        value={inputValue} 
+        onChange={handlerChange}/>
+
+        <span>{inputValue}</span>
+
+
+
+      </div>
     </div>
   );
 }
